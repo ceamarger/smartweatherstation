12c31872eec27d785e5de9629d2d6500c99f44de
Cedric Amarger <cedric.amarger@gmail.com>
2020-03-14 15:35:43 +0100

    Add AbstractWeatherAPIAccess class's documentation

--- src/weather/abstractweatherapiaccess.h
+++ src/weather/abstractweatherapiaccess.h
@@ -6,6 +6,14 @@
 
 #include "weathersettings.h"
 
+/*!
+ * \brief An abstract class that provides basics to have differents weather APIs
+ *
+ * If a class inherits from AbstractWeatherAPIAccess, make sure to overwrite \a requestData().
+ * Moreover, it is your own responsibility to emit or not the \a dataUpdated() signal.
+ * For now - but should change - this also your own responsibility to call \a requestData().
+ */
+
 class AbstractWeatherAPIAccess : public QObject {
     Q_OBJECT
 
@@ -19,6 +27,10 @@ public:
     WeatherSettings* settings() const { return m_settings; }
 
 signals:
+    /*!
+     * \brief Signal to emit when data has been received from the API
+     * \param data json data to transmit
+     */
     void dataUpdated(QJsonDocument data);
 
 private:
